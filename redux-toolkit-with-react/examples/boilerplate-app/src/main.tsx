84600d2ef5cd1c5ce952d7fd0701dfd174dcf974
gev-davtyan-rf <gevorgdavtyan99@mail.ru>
2026-06-11 14:52:57 +0400

    change folder name

---
+++ redux-toolkit-with-react/examples/boilerplate-app/src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import { Provider } from "react-redux";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    {/* <Provider store={store}> */}
+    <div>app</div>
+    {/* </Provider> */}
+  </StrictMode>,
+);
